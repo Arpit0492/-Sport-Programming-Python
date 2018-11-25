18bdda948059efa252062f95a8cca6b184c6a53e
Arpit Johri <johri.arpit@gmail.com>
2018-11-25 09:15:21 +0530

    HE Nov'18 WIP

---
+++ Sport-Programming-with-Python/HackerEarth/Circuits/Nov'18/Addition_Errors.py
@@ -0,0 +1,23 @@
+# n, k = [int(x) for x in raw_input().strip().split(' ')]
+
+def add_error(A, B):
+    s_a = str(A)
+    s_b = str(B)
+    ans = 0
+    for aa in range(len(s_a)-1, -1, -1):
+        
+
+    return ans
+
+
+
+T = int(raw_input())
+
+for _ in range(T):
+    A = int(raw_input())
+    B = int(raw_input())
+
+    actual_sum = A + B
+    bob_sum = add_error(A,B)
+
+    # print actual_sum
